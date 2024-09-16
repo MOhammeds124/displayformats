6916282f9f20de5dec776e9d4d63f8e733110fe3
MOhammeds124 <147172468+MOhammeds124@users.noreply.github.com>
2024-09-16 10:20:22 -0400

    Add files via upload

---
+++ displayformats/public/script.js
@@ -0,0 +1,7 @@
+
+function newFunction() 
+    {
+        return "i am albainian virus, due to poor funding i cant delete any important files. if you could be so kind, pls delete one of your important files then forward me to one of your friends"
+    }
+document.write(newFunction());
+	
